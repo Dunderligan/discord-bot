18c5c2af6c107e9deda031a5cf26c9cc96fad94d
JidOsc <84289227+JidOsc@users.noreply.github.com>
2025-11-03 22:24:41 +0100

    Download and save thumbnails for teams

--- bot.py
+++ bot.py
@@ -147,12 +147,18 @@ def get_teams(season: str) -> dict:
     return teams
 
 def format_name(name: str) -> str:
-    allowed_characters = "abcdefghijklmnopqrstuvwxyzåäö0123456789-"
+    allowed_characters = "abcdefghijklmnopqrstuvwxyz0123456789-"
     formatted_name = ""
     for c in name:
         char = c.lower()
         if char in allowed_characters:
             formatted_name += char
+        elif char in "åäáà":
+            formatted_name += "a"
+        elif char == "öõ":
+            formatted_name += "o"
+        elif char == "'" or formatted_name[-1] == "-":
+            continue
         else:
             formatted_name += "-"
     return formatted_name.lower()
@@ -169,6 +175,19 @@ def get_role_permissions(guild: discord.Guild, role: discord.Role) -> dict:
 def get_team_logo_link(team: str, size: int) -> str:
     return f'https://cdn.kesomannen.com/cdn-cgi/image/format=png,fit=scale-down,width={size}/dunderligan/logos/{team}.png'
 
+def get_team_thumbnail(team: str) -> str:
+    image_path = f"team_thumbnails/{format_name(team)}.png"
+    if not os.path.isfile(image_path):
+        image_formats = ("image/png", "image/jpeg", "image/jpg")
+        r = requests.get(get_team_logo_link(team, 32))
+        if r.headers["content-type"] in image_formats:
+            image = r.content
+            with open(image_path, "wb") as file:
+                file.write(image)
+        else:
+            return "/team_thumbnails/placeholder-team.jpg"
+    return image_path
+
 @tree.command(
         name='output_standing', 
         description='Outputs standing for division', 
@@ -181,7 +200,9 @@ async def output_standing(interaction: discord.Interaction, division: int) -> No
                         m.team_a_score,
                         m.team_b_score,
                         ra.name AS "roster_a_name",
-                        rb.name AS "roster_b_name"
+                        rb.name AS "roster_b_name",
+                        ra.id AS "roster_a_id",
+                        rb.id AS "roster_b_id"
                     FROM division d
                         JOIN "group" g ON g.division_id = d.id
                         JOIN "match" m ON m.group_id = g.id
@@ -193,23 +214,32 @@ async def output_standing(interaction: discord.Interaction, division: int) -> No
                    """)
     matches = cursor.fetchall()
 
-    scores: dict[str: int] = {}
+    teams: dict[str: dict[str: int]] = {} # första int är teams, andra id
     for match in matches:
         first_team = match[3]
         second_team = match[4]
         first_points = match[1]
         second_points = match[2]
 
-        scores[first_team] = scores[first_team] + first_points if scores.get(first_team) else first_points
-        scores[second_team] = scores[second_team] + second_points if scores.get(second_team) else second_points
+        if not teams.get(first_team):
+            teams[first_team] = {"score": 0, "id": 0}
+        if not teams.get(second_team):
+            teams[second_team] = {"score": 0, "id": 0}
+
+        teams[first_team]["score"] = teams[first_team]["score"] + first_points if teams.get(first_team) else first_points
+        teams[second_team]["score"] = teams[second_team]["score"] + second_points if teams.get(second_team) else second_points
+
+        teams[first_team]["id"] = match[5]
+        teams[second_team]["id"] = match[6]
     
-    sorted_scores = {}
-    for team in sorted(scores, key = scores.get, reverse = True):
-        sorted_scores[team] = scores[team]
+    sorted_teams = {}
+    for team in sorted(teams, key = lambda t: teams.get(t)["score"], reverse = True):
+        sorted_teams[team] = teams[team]
 
     standings = []
-    for team, score in sorted_scores.items():
-        standings.append((team, f"{score}/{9-score}/0", f"{score}p"))
+    for team, data in sorted_teams.items():
+        (score, id) = (data["score"], data["id"])
+        standings.append((get_team_thumbnail(id), team, f"{score}/{9-score}/0", f"{score}p"))
     # "standings": [(namn, x/x/x, xp)]
 
     current_time = str(datetime.datetime.now().strftime("%Y-%m-%d, %H:%M"))
@@ -222,12 +252,15 @@ async def output_standing(interaction: discord.Interaction, division: int) -> No
     sys_inputs = {"document_data": json.dumps(document_data), "time": json.dumps(current_time)}
 
     TYPST_FILE = f"standings.typ"
-    OUTPUT_FILE = f"standing-div-{division}.png" 
+    OUTPUT_FILE = f"generated_images/standing-div-{division}.png" 
     typst.compile(input = TYPST_FILE, output = OUTPUT_FILE, format = "png", sys_inputs = sys_inputs, ppi = 144.0)
     with open(OUTPUT_FILE, 'rb') as image:
         await interaction.channel.send(file = discord.File(image))
 
     await interaction.followup.send("Sent standings!")
+
+async def print_rosters(interaction: discord.Interaction, division: int) -> None:
+    pass
 
 ########## OLD CODE vvv
 """
@@ -262,44 +295,44 @@ async def print_standing(interaction: discord.Interaction, season: app_commands.
                    ')
     matches = cursor.fetchall()
 
-    division_scores: dict[str: dict[str: int]] = {}
+    division_teams: dict[str: dict[str: int]] = {}
     for match in matches:
-        if not match[0] in division_scores:
-            division_scores[match[0]] = {}
+        if not match[0] in division_teams:
+            division_teams[match[0]] = {}
 
-        if division_scores[match[0]].get(match[3]):
-            division_scores[match[0]][match[3]] += match[1]
+        if division_teams[match[0]].get(match[3]):
+            division_teams[match[0]][match[3]] += match[1]
         else:
-            division_scores[match[0]][match[3]] = match[1]
+            division_teams[match[0]][match[3]] = match[1]
 
-        if division_scores[match[0]].get(match[4]):
-            division_scores[match[0]][match[4]] += match[2]
+        if division_teams[match[0]].get(match[4]):
+            division_teams[match[0]][match[4]] += match[2]
         else:
-            division_scores[match[0]][match[4]] = match[2]
+            division_teams[match[0]][match[4]] = match[2]
     
     tables: dict[str: str] = {}
-    for division in division_scores:
-        sorted_scores = {}
-        for key in sorted(division_scores[division], key=division_scores[division].get, reverse=True):
-            sorted_scores[key] = division_scores[division][key]
-        division_scores[division] = sorted_scores
+    for division in division_teams:
+        sorted_teams = {}
+        for key in sorted(division_teams[division], key=division_teams[division].get, reverse=True):
+            sorted_teams[key] = division_teams[division][key]
+        division_teams[division] = sorted_teams
 
         #tables[division] = '
         embed: discord.Embed = discord.Embed(title=f'Säsong {season} - {division}')
         teams: str = '
-        scores: str = '
+        teams: str = '
         winlossdraws: str = '
 
-        for team in division_scores[division]:
-            score = division_scores[division][team]
+        for team in division_teams[division]:
+            score = division_teams[division][team]
             #tables[division] += f'{team}: {score} poäng | {score}W | {9-score}L\n'
             teams += f'{team}\n'
             winlossdraws += f'{score}-{9-score}-0\n'
-            scores += f'{score}p\n'
+            teams += f'{score}p\n'
             
         embed.add_field(name='Lag', value=f'{teams}', inline=True)
         embed.add_field(name='W/L/D', value=f'{winlossdraws}', inline=True)
-        embed.add_field(name='Poäng', value=f'{scores}', inline=True)
+        embed.add_field(name='Poäng', value=f'{teams}', inline=True)
         embed.timestamp = datetime.datetime.now()
         await interaction.channel.send(embed=embed)
 
--- standings.typ
+++ standings.typ
@@ -5,14 +5,16 @@
 )
 #set text(
   font: "Rajdhani",
-  size: 18pt
+  size: 18pt,
+  fill: rgb("#1f2937")
 )
 
-#show heading.where(level: 1): set text(size: 40pt)
-#show heading.where(level: 2): set text(size: 30pt)
+#show heading.where(level: 1): set text(size: 40pt, font: "Inter 28pt", weight: "semibold")
+#show heading.where(level: 2): set text(size: 26pt, font: "Inter 28pt", weight: "semibold", style: "italic")
 
 #show table.cell.where(y: 0): set text(weight: "bold")
 
+//#let document_data = ("standings": (("Omedvetna Pappertussarna", "9/0/0", "9p"), ("Cool Sharks", "9/0/0", "9p"), ("Oklippta Gamers", "6/3/0", "6p"), ("Flexibla Björnarna", "6/3/0", "6p"), ("Läskiga Hajarna", "3/6/0", "3p"), ("Mogna Pojkarna", "3/6/0", "3p"), ("Rika Gamers", "0/9/0", "0p"), ("Starka Pappertussarna", "0/9/0", "0p")), "division": 1, "season": "7")
 #let document_data = json(bytes(sys.inputs.document_data))
 #grid(
   columns: (1fr, auto),
@@ -27,16 +29,17 @@
   columns: (auto, 1fr, auto, auto),
   stroke: none,
   align: (horizon+center, horizon, horizon+center, horizon+center),
-  fill: (_, y) => if y > 0 {rgb("eceaf2")},
+  fill: (_, y) => if y > 0 {rgb("#e5e7eb")},
   row-gutter: 6pt,
   table.header(
     [Lag], [], [W/L/D], [Poäng]
   ),
-  ..for (team, result, points) in document_data.at("standings") {
-    (image("logo.png", height: 24pt), team, result, points)
+  ..for (team_logo, team, result, points) in document_data.at("standings") {
+    (image(team_logo, height: 24pt), team, result, points)
   }
 )
 #v(1fr)
 
+//#let current_time = "2025-11-03, 18.04"
 #let current_time = json(bytes(sys.inputs.time))
-\@ Dunderligan - Uppdaterad #current_time
+\@Dunderligan - Uppdaterad #current_time
