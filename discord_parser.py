b35c6817e59bfd9d2166a1a439e5b7f338008377
jidosc <oscar.jidemo@hotmail.se>
2025-12-16 22:01:28 +0100

    more robust. dont work for s7

--- discord_parser.py
+++ discord_parser.py
@@ -23,47 +23,7 @@ async def on_ready():
     await tree.sync(guild=discord.Object(id=server_id))
 
 
-async def parse_teams(
-    season: dict, category: discord.CategoryChannel
-) -> dict:
-    """
-    "season": 0,
-    "start_date": ?,
-    "legacy_ranks": False,
-    "divisions": {
-        "1": {
-            "groups": {
-                "Grupp A": {
-                    "teams": [
-                        "Missarna": {
-                            "players": [
-                                {
-                                "battletag": "B",
-                                "rank": 0,
-                                "tier": 0,
-                                "sr": 0000,
-                                "role": "dps",
-                                "is_captain": False,
-                                }
-                            ],
-                        }
-                    ],
-                    "matches": [
-                        {
-                        "date": None
-                        "rosterA": "Missarna",
-                        "rosterB": "Kaninerna",
-                        "teamAScore": 3,
-                        "teamBScore": 0,
-                        "draws": 0
-                        }
-                    ]
-                }
-            }
-        }
-    }
-    """
-    
+async def parse_teams(season: dict, category: discord.CategoryChannel) -> dict:
     TEAM: dict = {"players": []}
     PLAYER: dict = {
         "battletag": "",
@@ -78,10 +38,15 @@ async def parse_teams(
     STEPS: list = ["RANK", "ROLE", "BATTLETAG", "CAPTAIN"]
     season_with_teams = copy.deepcopy(season)
 
+    invalid_count = 0
     for channel in category.text_channels:
         if "spelartrupper" in channel.name:
             channel_info = channel.name.split("-")
-            division = "Division " + channel_info[2][0]
+            div = channel_info[2]
+            if div == "dl":
+                division = "Dunderligan"
+            else:
+                division = "Division " + channel_info[2][0]
 
             async for message in channel.history(limit=16):
                 content = message.content
@@ -119,7 +84,9 @@ async def parse_teams(
                         if team_name in value["teams"]:
                             group_name = group_name
                             break
-                        elif team_name.lower() in list(map(lambda s: s.lower(), value["teams"].keys())):
+                        elif team_name.lower() in list(
+                            map(lambda s: s.lower(), value["teams"].keys())
+                        ):
                             for team in value["teams"].keys():
                                 if team_name.lower() == team.lower():
                                     team_name = team
@@ -142,9 +109,13 @@ async def parse_teams(
                     player = list(
                         filter(
                             lambda p: p != "-" and p != "," and p != "",
-                            split_on_multiple(strip_emojis(roster_player), " ", ",", "-"),
+                            map(
+                                strip_emojis,
+                                split_on_multiple(roster_player, " ", ",", "-"),
+                            ),
                         )
                     )
+
                     member = copy.deepcopy(PLAYER)
                     current_step = 0
 
@@ -152,7 +123,9 @@ async def parse_teams(
                         if not player:
                             break
 
-                        if not is_valid(player[0]):
+                        if not is_valid(player[0]) or (
+                            current_step >= 1 and len(player[0]) == 1
+                        ):
                             player = player[1:]
 
                         elif STEPS[current_step] == "RANK":
@@ -160,7 +133,8 @@ async def parse_teams(
 
                             if rank[0]:
                                 member["sr"] = rank[1][0]
-                                season_with_teams["legacy_ranks"] = True
+                                if member["sr"] is not None:
+                                    season_with_teams["legacy_ranks"] = True
                             else:
                                 if len(rank[1]) == 2:
                                     member["rank"] = rank[1][0]
@@ -179,6 +153,18 @@ async def parse_teams(
 
                         elif STEPS[current_step] == "BATTLETAG":
                             member["battletag"] = strip_punc(player[0])
+                            ROLES: list = [
+                                "dps",
+                                "damage",
+                                "support",
+                                "coach",
+                                "tank",
+                                "flex"
+                            ]
+                            if member["battletag"].lower() in ROLES:
+                                member["battletag"] = f"InvalidNumber{invalid_count}#99999999"
+                                invalid_count += 1
+
                             player = player[1:]
                             current_step += 1
 
@@ -187,57 +173,19 @@ async def parse_teams(
                                 if "c" in player[0].lower():
                                     member["is_captain"] = True
                             break
-
                     team["players"].append(member)
     return season_with_teams
 
 
-async def parse_groups(
-    category: discord.CategoryChannel
-) -> dict:
-    """
-    "season": 0,
-    "start_date": ?,
-    "legacy_ranks": False,
-    "divisions": {
-        "1": {
-            "groups": {
-                "Grupp A": {
-                    "teams": [
-                        "Missarna": {
-                            "players": [
-                                {
-                                "battletag": "B",
-                                "rank": 0,
-                                "tier": 0,
-                                "sr": 0000,
-                                "role": "dps",
-                                "is_captain": False,
-                                }
-                            ],
-                        }
-                    ],
-                    "matches": [
-                        {
-                        "date": None
-                        "rosterA": "Missarna",
-                        "rosterB": "Kaninerna",
-                        "teamAScore": 3,
-                        "teamBScore": 0,
-                        "draws": 0
-                        }
-                    ]
-                }
-            }
-        }
+async def parse_groups(category: discord.CategoryChannel) -> dict:
+    GROUP_NAMES = ["Grupp A", "Grupp B", "Grupp C", "Grupp D"]
+
+    SEASON: dict = {
+        "season": 0,
+        "start_date": None,
+        "legacy_ranks": False,
+        "divisions": {},
     }
-    """
-
-    # GROUP NAMES GO FROM A -> B -> C -> ...
-    # MULTIPLE MESSAGES IN A CHANNEL MEANS ONE GROUP PER MESSAGE
-    # SPLIT ON "OMGÅNG", CAN GET DATE FROM THERE
-
-    SEASON: dict = {"season": 0, "start_date": None, "legacy_ranks": False, "divisions": {}}
     DIVISION: dict = {"groups": {}}
     GROUP: dict = {"teams": {}, "matches": []}
     TEAM: dict = {"players": []}
@@ -255,40 +203,51 @@ async def parse_groups(
     for channel in category.text_channels:
         if "spelschema" in channel.name:
             channel_info = channel.name.split("-")
-
             season["season"] = int(channel_info[-1][1])
-            division: str = ("Division " + channel_info[1][3])
+            div = channel_info[1]
+            if div == "dl":
+                division: str = "Dunderligan"
+            else:
+                division: str = "Division " + channel_info[1][3]
 
             if not season.get("divisions").get(division):
                 season["divisions"][division] = copy.deepcopy(DIVISION)
-            
+
             message_count = 0
             async for message in channel.history(limit=4):
                 if season.get("start_date") is None:
-                    start_date = message.created_at.replace(hour=19, minute=0, second=0, microsecond=0)
+                    start_date = message.created_at.replace(
+                        hour=19, minute=0, second=0, microsecond=0
+                    )
                     season["start_date"] = start_date.isoformat()
 
                 content = message.content
                 rows = [row for row in content.split("\n")]
 
-                group = copy.deepcopy(GROUP)
-
                 rounds = []
                 current_round = 0
                 current_group = 0
+                ready = False
+                checked_teams = 0
+                multiple_groups = False
                 for row in rows:
                     line = strip_punc(row)
-                    if line.lower().startswith("omgång") or line.lower().startswith("division"):
+                    if line.lower().startswith("omgång") or line.lower().startswith(
+                        "division"
+                    ):
                         rounds.append([])
-                        current_round += 1
+                        ready = True
+                        if not multiple_groups:
+                            current_round += 1
                         current_group = 0
                     elif current_round == 0:
                         continue
-                    elif line.isspace() or line == "":
+                    elif ready and (line.isspace() or line == "") and checked_teams >= 0:
                         current_group += 1
-
+                        multiple_groups = True
                     elif line != "" and not line.lower().startswith("senast"):
-                        line = line.split("vs.")
+                        line = split_on_multiple(line, "vs.", " - ", " – ")
+                        print(line)
 
                         rosterA: str = strip_punc(line[0])
                         rosterB: str = strip_punc(line[1])
@@ -296,37 +255,46 @@ async def parse_groups(
                         teamBScore: int = 0
                         draws: int = 0
 
-                        if "..." in line[1]:
-                            rest = line[1].split("...")
+                        if "..." in line[1] or "…" in line[1]:
+                            rest = split_on_multiple(line[1], "...", "…")
                             rosterB = strip_punc(rest[0])
-
                             score = list(map(strip_punc, rest[1].split("-")))
                             teamAScore = int(score[0][0])
                             teamBScore = int(score[1][0])
                             draws = 3 - (teamAScore + teamBScore)
 
-                        if rosterA not in group["teams"]:
-                            group["teams"][rosterA] = copy.deepcopy(TEAM)
-                        if rosterB not in group["teams"]:
-                            group["teams"][rosterB] = copy.deepcopy(TEAM)                            
-
                         match = copy.deepcopy(MATCH)
-
                         if start_date:
-                            match["date"] = (start_date + datetime.timedelta(days=7*(current_round - 1))).isoformat()
+                            match["date"] = (
+                                start_date
+                                + datetime.timedelta(days=7 * (current_round - 1))
+                            ).isoformat()
                         match["rosterA"] = rosterA
                         match["rosterB"] = rosterB
                         match["teamAScore"] = teamAScore
                         match["teamBScore"] = teamBScore
                         match["draws"] = draws
 
-                        group["matches"].append(match)
+                        group_number = max(current_group, message_count)
+                        if not season["divisions"][division]["groups"].get(
+                            GROUP_NAMES[group_number]
+                        ):
+                            season["divisions"][division]["groups"][
+                                GROUP_NAMES[group_number]
+                            ] = copy.deepcopy(GROUP)
 
-                LETTERS = ["Grupp A", "Grupp B", "Grupp C", "Grupp D"]
-                season["divisions"][division]["groups"][LETTERS[message_count]] = group
+                        group = season["divisions"][division]["groups"][
+                            GROUP_NAMES[group_number]
+                        ]
+                        group["matches"].append(match)
+                        if rosterA not in group["teams"]:
+                            group["teams"][rosterA] = copy.deepcopy(TEAM)
+                        if rosterB not in group["teams"]:
+                            group["teams"][rosterB] = copy.deepcopy(TEAM)
+
                 message_count += 1
     return season
-    
+
 
 def split_on_multiple(text: str, *separators):
     modified = text
@@ -361,6 +329,11 @@ def get_rank(parts: list) -> tuple[bool, list]:
     RANK_ALIASES: dict = {"guld": "gold", "gm": "grandmaster", "brons": "bronze"}
 
     rank = strip_punc(str(parts[0]).lower())
+    if rank.lower() == "n/a" or rank.lower() == "unranked":
+        if str(parts[1]).lower() == "n/a" or str(parts[1]).lower() == "unranked":
+            return (False, [None, None])
+        return (False, [None])
+
     if "." in rank or "k" in rank:
         try:
             sr = int(float(rank.strip("k ,-?").replace(",", ".")) * 1000)
@@ -369,7 +342,7 @@ def get_rank(parts: list) -> tuple[bool, list]:
         return (True, [sr])
     elif rank in RANKS or rank in RANK_ALIASES:
         if rank not in RANKS:
-            rank = RANK_ALIASES
+            rank = RANK_ALIASES[rank]
         try:
             tier = int(strip_punc(parts[1]))
         except ValueError:
@@ -384,7 +357,7 @@ def strip_punc(text: str) -> str:
     """
     Strips punctuation from text.
     """
-    return text.strip(" ,.-()*")
+    return text.strip(" ,.-()*|")
 
 
 def strip_emojis(text: str) -> str:
@@ -393,9 +366,9 @@ def strip_emojis(text: str) -> str:
     and end of string.
     """
     mod_text = text
-    if text.startswith(":"):
+    if mod_text.startswith(":") and mod_text.count(":") >= 2:
         mod_text = mod_text[mod_text.index(":", 1) + 1 :].lstrip()
-    if text.endswith(":"):
+    if mod_text.endswith(":") and mod_text.count(":") >= 2:
         mod_text = mod_text[: mod_text.index(":")].rstrip()
     return mod_text
 
@@ -405,10 +378,8 @@ def is_valid(text: str) -> bool:
     Returns whether text contains valid information, or
     is just wrongly placed punctuation.
     """
-    return (
-        (len(text) > 1 or text in "12345")
-        and not (text.startswith(":") and text.endswith(":"))
-        and text != "N/A"
+    return (len(text) > 1 or text in "12345") and not (
+        text.startswith(":") and text.endswith(":")
     )
 
 
@@ -418,14 +389,16 @@ def is_valid(text: str) -> bool:
     guild=discord.Object(id=server_id),
 )
 @app_commands.checks.has_role(admin_role_id)
-@app_commands.describe(category="Category to look for 'spelartrupper' and 'spelschema' in.")
+@app_commands.describe(
+    category="Category to look for 'spelartrupper' and 'spelschema' in."
+)
 async def parse_season(
     interaction: discord.Interaction, category: discord.CategoryChannel
 ):
     season_data = await parse_groups(category)
     season_data = await parse_teams(season_data, category)
     season: int = season_data["season"]
-    
+
     with open(f"parsed_data/season_{season}.json", "w") as file:
         json.dump(season_data, file, indent=1)
 
