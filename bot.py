b975a743b738249a32300544cfeb6f5a05d9057f
jidosc <oscar.jidemo@hotmail.se>
2026-04-28 15:44:41 +0200

    youtube checking

--- bot.py
+++ bot.py
@@ -2,13 +2,12 @@
 import discord
 import os
 
-# TODO switch to https://pypi.org/project/asyncpg/
-import requests
 import datetime
 import asyncio
 import sqlite3
 
 import db
+import youtube_integration
 
 from dotenv import load_dotenv
 from discord import app_commands
@@ -17,10 +16,9 @@ load_dotenv()
 try:
     token = os.getenv("TOKEN")
     server_id: int = int(os.getenv("SERVER_ID"))
-    admin_role_id: int = int(os.getenv("ADMIN_ID"))
-    mod_role_id: int = int(os.getenv("MOD_ID"))
-    observer_role_id: int = int(os.getenv("OBSERVER_ID"))
-    captains_role_id: int = int(os.getenv("CAPTAINS_ID"))
+    yt_token = os.getenv("YOUTUBE_API_KEY")
+    yt_channel_id = os.getenv("YOUTUBE_CHANNEL_ID")
+    yt_notification_channel_id = os.getenv("YOUTUBE_NOTIFICATION_CHANNEL_ID")
 except Exception as e:
     print(f"Error loading environment variables: {e}")
     exit(1)
@@ -31,439 +29,50 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 db_connection: sqlite3.Connection
+yt_integration = youtube_integration.YoutubeIntegration(yt_token)
+yt_integration.monitor_channel(yt_channel_id)
+
+
+@client.event
+async def on_new_videos(videos):
+    """Callback function that is called when new videos are detected on the monitored YouTube channel."""
+    channel = discord.utils.get(
+        client.get_all_channels(), id=int(yt_notification_channel_id)
+    )
+    if channel is None:
+        print(
+            f"Could not find channel with ID {yt_notification_channel_id}. Please check the ID and try again."
+        )
+        return
+    for video in videos:
+        await channel.send(
+            f"En ny video publicerades precis på Dunderligans kanal: **{video.title}**\n{video.url}"
+        )
 
 
 @client.event
 async def on_ready():
+    """Called when the bot is ready."""
     print(f"We have logged in as {client.user}")
+    yt_integration.add_new_video_callback(on_new_videos)
     await tree.sync(guild=discord.Object(id=server_id))
 
 
-@tree.command(
-    name="create_new_objects",
-    description="Creates channels and roles for all team in a season. Gets data from dunderligan.se API.",
-    guild=discord.Object(id=server_id),
-)
-@app_commands.checks.has_role(admin_role_id)
-async def create_new_objects(interaction: discord.Interaction) -> None:
-    await interaction.response.defer()
-    seasons = await request_seasons()
-    if seasons is None:
-        await interaction.followup.send("Failed to fetch seasons.")
-        return
-
-    season_name = "Säsong 8"
-
-    for season in seasons:
-        print(f"{season['name']}")
-        if season["name"] != season_name:
-            print("Not correct.")
-            continue
-        season_slug = season["slug"]
-        season_slug = season_slug[0] + season_slug[-1]
-        for division in season["divisions"]:
-            print(f"{division['name']}")
-
-            division_name = division["name"]
-            division_slug = division["slug"]
-            if division_slug == "dunderserien":
-                division_slug = "ds"
-            else:
-                division_slug = "div" + division_slug[0]
-
-            division_roles = {}
-            for group in division["groups"]:
-                for team in group["rosters"]:
-                    team_name = team["name"]
-                    previously_created_role = db_connection.execute("SELECT id FROM channels WHERE type = 3 AND team = ?", (team["id"],)).fetchone()
-                    if previously_created_role is not None:
-                        division_roles[team_name] = discord.utils.find(
-                            lambda r: r.id == previously_created_role[0], interaction.guild.roles
-                        )
-                        continue
-
-                    team_slug = team["slug"]
-                    print(f"Starts creating role for team: {team_name}")
-                    team_role: discord.Role = await interaction.guild.create_role(
-                        name=team_name
-                    )
-                    print(f"Finished creating role for team: {team_name}")
-                    db_connection.execute(
-                        "INSERT INTO channels (id, type, season, division, team) VALUES (?, ?, ?, ?, ?)",
-                        (team_role.id, 3, season_name, division_name, team["id"]),
-                    )
-                    db_connection.commit()
-                    division_roles[team_name] = team_role
-                    print(f"Created role for team: {team_name}")
-
-            print("Starts creating divisions permissions.")
-            division_permissions = get_division_permissions(
-                interaction.guild, division_roles.values()
-            )
-
-            print("Starts creating channels for division.")
-            info_category: discord.CategoryChannel = await create_channel(
-                interaction.guild,
-                0,
-                f"{season_name} - {division_name}",
-                season_slug,
-                division_slug,
-                None,
-                None,
-                division_permissions,
-            )
-            print(f"Created info_category: {info_category.name}")
-            print(f"Attempting to create spelschema channel with overwrites: {division_permissions}")
-            await create_channel(
-                interaction.guild,
-                1,
-                f"spelschema-{division_slug}-{season_slug}",
-                season_slug,
-                division_slug,
-                None,
-                info_category,
-                division_permissions,
-            )
-            print("Created spelschema channel")
-            await create_channel(
-                interaction.guild,
-                1,
-                f"tabell-{division_slug}-{season_slug}",
-                season_slug,
-                division_slug,
-                None,
-                info_category,
-                division_permissions,
-            )
-            await create_channel(
-                interaction.guild,
-                1,
-                f"information-{division_slug}-{season_slug}",
-                season_slug,
-                division_slug,
-                None,
-                info_category,
-                division_permissions,
-            )
-            await create_channel(
-                interaction.guild,
-                1,
-                f"spelartrupper-{division_slug}-{season_slug}",
-                season_slug,
-                division_slug,
-                None,
-                info_category,
-                division_permissions,
-            )
-            await create_channel(
-                interaction.guild,
-                1,
-                f"rapportera-bans-{division_slug}-{season_slug}",
-                season_slug,
-                division_slug,
-                None,
-                info_category,
-                get_captains_permissions(interaction.guild, division_roles.values()),
-            )
-            text_category: discord.CategoryChannel = await create_channel(
-                interaction.guild,
-                0,
-                f"{season_name} - {division_name} - Klubbhus",
-                season_slug,
-                division_slug,
-                None,
-                None,
-                get_admin_permissions(interaction.guild),
-            )
-            voice_category: discord.CategoryChannel = await create_channel(
-                interaction.guild,
-                0,
-                f"{season_name} - {division_name} - Röstkanaler",
-                season_slug,
-                division_slug,
-                None,
-                None,
-                get_admin_permissions(interaction.guild),
-            )
-
-            for group in division["groups"]:
-                for team in group["rosters"]:
-                    team_name = team["name"]
-                    team_slug = team["slug"]
-                    team_id = team["id"]
-
-                    team_role = division_roles[team_name]
-                    team_permissions = get_team_permissions(
-                        interaction.guild, team_role
-                    )
-                    text_channel = await create_channel(
-                        interaction.guild,
-                        1,
-                        f"{team_slug}",
-                        season_slug,
-                        division_slug,
-                        team_id,
-                        text_category,
-                        team_permissions,
-                    )
-                    await text_channel.send(f"Välkommen till klubbhuset för {team_name}!\nHär kan ni kommunicera inom laget, men det kommer också vara här som ligaledningen kan ta kontakt med er.")
-
-                    await create_channel(
-                        interaction.guild,
-                        2,
-                        f"{team_name}",
-                        season_slug,
-                        division_slug,
-                        team_id,
-                        voice_category,
-                        team_permissions,
-                    )
-
-            print(f"Finished creating objects for division: {division['name']}")
-        break
-    await interaction.followup.send("Finished creating new objects.")
-
-
-async def create_channel(
-    guild: discord.Guild,
-    type: int,
-    name: str,
-    season: str,
-    division: str,
-    team: str,
-    category: discord.CategoryChannel,
-    overwrites: dict,
-):
-    channel = None
-    try:
-        if type == 0:
-            if overwrites is not None:
-                channel = await guild.create_category(name=name, overwrites=overwrites)
-            else:
-                channel = await guild.create_category(name=name)
-        elif type == 1:
-            channel = await guild.create_text_channel(
-                name=name, category=category, overwrites=overwrites
-            )
-        elif type == 2:
-            channel = await guild.create_voice_channel(
-                name=name, category=category, overwrites=overwrites
-            )
-
-        db_connection.execute(
-            "INSERT INTO channels (id, type, season, division, team) VALUES (?, ?, ?, ?, ?)",
-            (channel.id, type, season, division, team),
-        )
-        db_connection.commit()
-        await asyncio.sleep(0.2)  # Sleep to avoid hitting rate limits
-        return channel
-    except Exception as e:
-        print(f"Error creating channel {name}: {e}")
-        return None
-
-
-@tree.command(
-    name="delete_all_objects",
-    description="Deletes all channels and roles previously created by the bot.",
-    guild=discord.Object(id=server_id),
-)
-@app_commands.checks.has_role(admin_role_id)
-async def delete_all_objects(interaction: discord.Interaction) -> None:
-    await interaction.response.defer()
-    cursor = db_connection.cursor()
-    cursor.execute("SELECT id, type FROM channels")
-    channels = cursor.fetchall()
-    for id, type in channels:
-        await asyncio.sleep(0.2)  # Sleep to avoid hitting rate limits
-        channel = interaction.guild.get_channel(id)
-        if channel is not None:
-            await channel.delete()
-            print(f"Deleted channel with id: {id}")
-        else:
-            role = interaction.guild.get_role(id)
-            if role is not None:
-                await role.delete()
-                print(f"Deleted role with id: {id}")
-            else:
-                print(f"Could not find channel or role with id: {id}")
-    db_connection.execute("DELETE FROM channels")
-    db_connection.commit()
-    await interaction.followup.send("Finished deleting all objects.")
-
-async def request_seasons():
-    try:
-        url = os.getenv("SEASONS_URL")
-        json_data = requests.get(url, timeout=10).json().get("results")
-        return json_data
-    except Exception as e:
-        print(f"Error fetching seasons: {e}")
-        return None
-
-
-WRITE_PERMISSIONS: discord.PermissionOverwrite = discord.PermissionOverwrite(
-    view_channel=True, connect=True, send_messages=True, read_message_history=True
-)
-READ_PERMISSIONS: discord.PermissionOverwrite = discord.PermissionOverwrite(
-    view_channel=True, connect=True, send_messages=False, read_message_history=True
-)
-NO_PERMISSIONS: discord.PermissionOverwrite = discord.PermissionOverwrite(
-    view_channel=False, connect=False, send_messages=False, read_message_history=False
-)
-ONLY_WRITE_PERMISSIONS: discord.PermissionOverwrite = discord.PermissionOverwrite(
-    connect=True, send_messages=True
-)
-
-
-def get_admin_permissions(guild: discord.Guild) -> dict:
-    bot_role: discord.Role = discord.utils.find(
-        lambda r: r.name == "Dunderbot", guild.roles
-    )
-    admin_role: discord.Role = discord.utils.find(
-        lambda r: r.id == admin_role_id, guild.roles
-    )
-    overwrites: dict[discord.Role, discord.PermissionOverwrite] = {
-        guild.default_role: NO_PERMISSIONS,
-        bot_role: WRITE_PERMISSIONS,
-        admin_role: WRITE_PERMISSIONS,
-    }
-    return overwrites
-
-
-def get_team_permissions(guild: discord.Guild, team_role: discord.Role) -> dict:
-    overwrites = get_admin_permissions(guild)
-    overwrites[team_role] = WRITE_PERMISSIONS
-    return overwrites
-
-
-def get_division_permissions(guild: discord.Guild, division_roles) -> dict:
-    observer_role: discord.Role = discord.utils.find(
-        lambda r: r.id == observer_role_id, guild.roles
-    )
-    mod_role: discord.Role = discord.utils.find(
-        lambda r: r.id == mod_role_id, guild.roles
-    )
-    overwrites = get_admin_permissions(guild)
-    if mod_role is not None:
-        overwrites[mod_role] = WRITE_PERMISSIONS
-    if observer_role is not None:
-        overwrites[observer_role] = READ_PERMISSIONS
-    for role in division_roles:
-        overwrites[role] = READ_PERMISSIONS
-    return overwrites
-
-def get_captains_permissions(guild: discord.Guild, division_roles) -> dict:
-    captains_role: discord.Role = discord.utils.find(
-        lambda r: r.id == captains_role_id, guild.roles
-    )
-    overwrites = get_division_permissions(guild, division_roles)
-    if captains_role is not None:
-        overwrites[captains_role] = ONLY_WRITE_PERMISSIONS
-    return overwrites
-
-
-@tree.command(
-    name="print_rosters",
-    description="Prints rosters for all team in division",
-    guild=discord.Object(id=server_id),
-)
-async def print_rosters(interaction: discord.Interaction) -> None:
-    await interaction.response.defer()
-    
-    # Load output.json
-    import json
-    try:
-        with open("output.json", "r", encoding="utf-8") as f:
-            data = json.load(f)
-    except Exception as e:
-        await interaction.followup.send(f"Failed to load output.json: {e}")
-        return
-    
-    try:
-        divisions = data.get("divisions", {})
-        for division_name, division_data in divisions.items():
-            
-            groups = division_data.get("groups", {})
-            for group_name, group_data in groups.items():
-                
-                teams = group_data.get("teams", {})
-                for team_name, team_data in teams.items():
-                    players = team_data.get("players", [])
-                    
-                    # Build team roster
-                    team_msg = f"## {team_name}\n"
-
-                    roles = {"tank": 0, "damage": 1, "support": 2, "flex": 3, "coach": 4, "manager": 5}
-                    ranks = {"champion": "champion", "grandmaster": "gm", "master": "master", "diamond": "dia", "platinum": "plat", "gold": "guld", "silver": "silver", "bronze": "bronze"}
-                    players.sort(key=lambda p: roles.get(p.get("role", "flex"), 3))  # Default to flex if role is unknown
-                    on_staff = False
-                    
-                    for player in players:
-                        battletag = player.get("battletag", "Unknown")
-                        role = player.get("role", "N/A")
-                        if role in ["coach", "manager"] and not on_staff:
-                            team_msg += "\n"
-                            on_staff = True
-
-                        rank = player.get("rank", "N/A")
-                        tier = player.get("tier", "N/A")
-                        is_captain = player.get("is_captain", False)
-                        
-                        captain_mark = "**C**" if is_captain else ""
-
-                        rank_emote = discord.utils.get(interaction.guild.emojis, name=str(ranks.get(rank)).lower())
-                        role_emote = discord.utils.get(interaction.guild.emojis, name=str(role).lower())
-
-                        if role in ["coach", "manager"]:
-                            team_msg += f"{role_emote} {str(role).capitalize()} - {str(battletag).split("#")[0]}\n"
-                        elif rank and tier:
-                            team_msg += f"{rank_emote} {role_emote} *{str(rank).capitalize()} {tier}, {str(role).capitalize()}* - {f'__{battletag}__' if captain_mark else str(battletag).split("#")[0]} {captain_mark}\n"
-                        elif rank:
-                            team_msg += f"{rank_emote} {role_emote} *{str(rank).capitalize()}, {str(role).capitalize()}* - {battletag} **{captain_mark}**\n"
-                        else:
-                            team_msg += f"{role_emote} *Unranked, {str(role).capitalize()}* - {battletag} **{captain_mark}**\n"
-                    
-                    await interaction.channel.send(team_msg)
-    except Exception as e:
-        await interaction.followup.send(f"Failed to process rosters: {e}")
-        return
-    await interaction.followup.send("Completed.")
-
-
-@tree.command(
-    name="update_voice_channels",
-    description="Updates voice channels for all teams in division",
-    guild=discord.Object(id=server_id),
-)
-async def update_voice_channels(interaction: discord.Interaction) -> None:
-    await interaction.response.defer()
-
-    voice_channels = db_connection.execute("SELECT id, team FROM channels WHERE type = 2").fetchall()
-    for channel_id, team_id in voice_channels:
-        channel = interaction.guild.get_channel(channel_id)
-        if channel is None:
-            print(f"Could not find channel with id: {channel_id}")
-            continue
-        await channel.edit(user_limit=5)
-        print(f"Updated voice channel with id: {channel_id}")
-
-    await interaction.followup.send("Completed.")
-
-
 async def check_updates():
+    """Checks for updates on YouTube channel every hour."""
     while True:
-        await asyncio.sleep(5)
+        await asyncio.sleep(3600)  # Updates every hour
         print(f"Checking for updates at {datetime.datetime.now()}")
+        await yt_integration.check_for_new_videos()
 
 
 async def main():
     """Runs client that checks for user-commands and server-side updates in parallell"""
-    # await request_seasons()
-
     global db_connection
     db_connection = db.get_db_connection()
     db.set_up_db()
 
-    await asyncio.gather(client.start(token))
+    await asyncio.gather(client.start(token), check_updates())
 
 
 asyncio.run(main())
---
+++ youtube_integration.py
@@ -0,0 +1,72 @@
+import requests
+import json
+from datetime import datetime
+
+YOUTUBE_LINK = "https://youtu.be/ID"
+last_time_checked = datetime.now().astimezone(None)
+
+class YoutubeIntegration:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.monitored_channels = []
+        self.callbacks = []
+        # Initialize YouTube API client here
+
+    def monitor_channel(self, channel_id):
+        # Code to monitor the specified YouTube channel for new videos
+        self.monitored_channels.append(channel_id)
+
+
+    def add_new_video_callback(self, callback):
+        # Code to set a callback function that will be called when a new video is detected
+        self.callbacks.append(callback)
+
+
+    async def fetch_latest_videos(self, channel_id):
+        # Code to fetch the latest videos from the specified channel using YouTube API
+        # This should return a list of YoutubeVideo objects
+        response = requests.get(f"https://www.googleapis.com/youtube/v3/search?key={self.api_key}&channelId={channel_id}&part=snippet,id&order=date&maxResults=8")
+        if response.ok:
+            with open("response.json", "+wb") as file:
+                # log content of response
+                file.write(response.content)
+
+            print("Request was successful")
+            # load bytes of response to json
+            return json.loads(response.content.decode('utf-8'))
+        print("Request unsuccesful")
+
+
+    async def check_for_new_videos(self):
+        # Code to check for new videos on the monitored channel
+        latest_videos = await self.fetch_latest_videos(self.monitored_channels[0])  # Assuming monitoring one channel for simplicity
+        new_videos: list[YoutubeVideo] = []
+        global last_time_checked
+
+        for video in latest_videos.get('items'):
+            video_snippet = video.get('snippet')
+            video_datetime: datetime = datetime.fromisoformat(video_snippet.get('publishedAt')).astimezone(None)
+            # if video is newer than last time it was checked
+            if video_datetime > last_time_checked:
+                # save dictionary with id and title
+                new_video = YoutubeVideo(
+                    video.get('id').get('videoId'), 
+                    video_snippet.get('title'), 
+                    video_snippet.get('description'), 
+                    YOUTUBE_LINK.replace("ID", video.get('id').get('videoId')))
+                new_videos.append(new_video)
+        
+        last_time_checked = datetime.now().astimezone(None)
+        for callback in self.callbacks:
+            await callback(new_videos)
+
+
+class YoutubeVideo:
+    def __init__(self, video_id, title, description, url):
+        self.video_id = video_id
+        self.title = title
+        self.description = description
+        self.url = url
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
